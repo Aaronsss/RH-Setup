0e483b2706123d059d4827308de0d1914661b417
Aaron <23297034+Aaronsss@users.noreply.github.com>
2023-04-11 21:36:59 +0100

    dont run script if RH is already installed

--- boot/RH/rh-setup.sh
+++ boot/RH/rh-setup.sh
@@ -1,20 +1,31 @@
-while [ "$(hostname -I)" = "" ]; do
-  echo "No network detected: $(date)"
-  sleep 2
-done
-echo "Network connection detected, running RH instal script $(date)"
-
-echo "Instal username is: $USER"
-
-cd ~
-if [ -d "./RH-Setup-main" ];
+if grep -Fxq "2" /boot/RH/RHInstalProgress.txt
 then
-    echo "Instal script already downloaded"
+    echo "$(date) RotorHazard has been installed, delete /boot/RH/RHInstalProgress.txt if you want the installer to run again"
+    if [ -d "./RH-Setup-main" ];
+    then
+      rm -R ./RH-Setup-main
+    fi
 else
-	echo "Downloading instal script"
-    wget https://github.com/Aaronsss/RH-Setup/archive/refs/heads/main.zip
-    unzip -q main.zip
-    chmod 744 ./RH-Setup-main/rh-pi-setup.sh
-fi
 
-./RH-Setup-main/rh-pi-setup.sh
+  while [ "$(hostname -I)" = "" ]; do
+    echo "$(date) No network detected"
+    sleep 2
+  done
+  echo "Network connection detected, running RH instal script $(date)"
+
+  echo "Instal username is: $USER"
+
+  cd ~
+  if [ -d "./RH-Setup-main" ];
+  then
+      echo "Instal script already downloaded"
+  else
+    echo "Downloading instal script"
+      wget https://github.com/Aaronsss/RH-Setup/archive/refs/heads/main.zip
+      unzip -q main.zip
+      chmod 744 ./RH-Setup-main/rh-pi-setup.sh
+  fi
+
+  ./RH-Setup-main/rh-pi-setup.sh
+
+  fi
